d3f7c13c43259990d62114125bc12554c269edf6
Mike Solomon <mike@wavr.so>
2022-03-25 10:58:01 +0200

    Adds extra helper function for when we already have an element

--- src/Deku/Toplevel.purs
+++ src/Deku/Toplevel.purs
@@ -15,9 +15,36 @@ import Effect (Effect)
 import FRP.Event (subscribe)
 import Prim.Row (class Nub)
 import Web.HTML (window)
+import Web.DOM.Element as Web.DOM
 import Web.HTML.HTMLDocument (body)
 import Web.HTML.HTMLElement (toElement)
 import Web.HTML.Window (document)
+
+runInElement
+  :: forall push sn graph control
+   . Create (root :: Element Root sn) () graph
+  => Nub graph graph
+  => Web.DOM.Element
+  -> ( (push -> Effect Unit) -> { | sn } /\ control
+     )
+  -> ( forall proofB
+        . push
+       -> control
+       -> MDOM RunDOM RunEngine proofB Unit graph control
+     )
+  -> Effect Unit
+runInElement elt scene loop = do
+  ffi <- makeFFIDOMSnapshot
+  void $ subscribe
+    ( run (pure unit) (pure unit) defaultOptions ffi
+        $
+          ( \_ push ->
+              let sn /\ ctrl = scene push in root elt sn /\ ctrl
+          ) @> \env ctrl -> case env of
+            Left _ -> pure ctrl
+            Right push -> loop push ctrl
+    )
+    (_.res >>> pure)
 
 runInBody
   :: forall push sn graph control
@@ -33,17 +60,6 @@ runInBody
   -> Effect Unit
 runInBody scene loop = do
   b' <- window >>= document >>= body
-  for_ (toElement <$> b') \elt -> do
-    ffi <- makeFFIDOMSnapshot
-    subscribe
-      ( run (pure unit) (pure unit) defaultOptions ffi
-          $
-            ( \_ push ->
-                let sn /\ ctrl = scene push in root elt sn /\ ctrl
-            ) @> \env ctrl -> case env of
-              Left _ -> pure ctrl
-              Right push -> loop push ctrl
-      )
-      (_.res >>> pure)
+  for_ (toElement <$> b') \elt -> runInElement elt scene loop
 
 infix 1 runInBody as 🚀
