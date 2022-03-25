cc99c6ba3c8ef431fb3438089faca96f17c8d877
Mike Solomon <mike@wavr.so>
2022-03-25 11:07:44 +0200

    Adds span

--- examples/toplevel/Toplevel.purs
+++ examples/toplevel/Toplevel.purs
@@ -17,20 +17,20 @@ main =
   ( \push -> u $
       ( (Proxy :: _ """
 <div ~mydiv~>
-  <h1>hello!</h1>
-  <p>this is what a no-frills deku app looks like</p>
-  <p>it is powered by pursx, a html-like format inspired by jsx</p>
+  <h1>Hello!</h1>
+  <p>This is what a no-frills deku app looks like.</p>
+  <p>It is powered by pursx, a html-like format inspired by JSX.</p>
   <h2>Why Deku</h2>
   <li>
-    <ul>It's fast</ul>
+    <ul>It's fast.</ul>
     <ul>
       Well, that's about it for now... it's fast,
       but perhaps it has other advantages!
     </ul>
   </li>
   <p>
-    Gratuitous demo: click the button below
-    to change the background of this div.
+    <span style="font-weight:800;">Gratuitous demo alert:</span>
+      click the button below to change the background of this div.
   </p>
   ~mybutton~
 </div>
