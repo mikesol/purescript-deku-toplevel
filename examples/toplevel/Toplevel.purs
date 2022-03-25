228add7fb1982851b64d2d6ce91b4656713ce9d9
Mike Solomon <mike@wavr.so>
2022-03-25 11:04:55 +0200

    Makes demo slightly longer

--- examples/toplevel/Toplevel.purs
+++ examples/toplevel/Toplevel.purs
@@ -15,12 +15,32 @@ import Type.Proxy (Proxy(..))
 main :: Effect Unit
 main =
   ( \push -> u $
-      ( (Proxy :: _ "<div ~mydiv~>hello!</div>") ~!
-          { mydiv: D.div'attr [ D.OnClick := cb (const $ push unit) ]
+      ( (Proxy :: _ """
+<div ~mydiv~>
+  <h1>hello!</h1>
+  <p>this is what a no-frills deku app looks like</p>
+  <p>it is powered by pursx, a html-like format inspired by jsx</p>
+  <h2>Why Deku</h2>
+  <li>
+    <ul>It's fast</ul>
+    <ul>
+      Well, that's about it for now... it's fast,
+      but perhaps it has other advantages!
+    </ul>
+  </li>
+  <p>
+    Gratuitous demo: click the button below
+    to change the background of this div.
+  </p>
+  ~mybutton~
+</div>
+""") ~!
+          { mydiv: D.div'attr []
+          , mybutton: D.button [ D.OnClick := cb (const $ push unit) ] {}
           }
       )
   ) 🚀
      \_ -> const $ change
         { "root.psx.mydiv": D.div'attr
-            [ D.Style := "background-color: rgb(65,72,109);" ]
+            [ D.Style := "background-color: rgb(195,212,209);" ]
         }
